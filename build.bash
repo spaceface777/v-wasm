de5fa581c0eac603f62a7f27ffc81be1fca4570a
spaceface777 <spaceface7777@gmail.com>
2020-05-25 13:47:52 +0200

    update build file

--- build.bash
+++ build.bash
@@ -2,11 +2,11 @@
 
 set -e
 
-emcc \
-	-std=gnu11 -w -D__linux__ \
+emcc -std=gnu11 -w -D__linux__ \
 	placeholders.c vc/v.c \
 	-lm \
-	--post-js index.js \
 	-s 'EXTRA_EXPORTED_RUNTIME_METHODS=["callMain"]' \
 	-s INVOKE_RUN=0 \
+	--post-js index.js \
+	--preload-file v/vlib@vlib \
 	-o public/index.js
